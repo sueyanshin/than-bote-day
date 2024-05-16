9c8cb7090fba4642a5d4f5af6d5106938dad5c2c
sueyanshin <sueyanshin@gmail.com>
2024-05-16 21:17:06 +0630

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://thanboteday.vercel.app",
-    }),
-  ],
-});
+  plugins: [react()],
+})
